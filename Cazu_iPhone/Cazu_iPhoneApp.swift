12b0b9cd47470b757e1656aed074e6a7d257eeea
Gustavo Anjos <gustavo.anjos@popcode.com.br>
2021-04-13 08:18:20 -0400

    Initial Commit

---
+++ Cazu_iPhone/Cazu_iPhoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cazu_iPhoneApp.swift
+//  Cazu_iPhone
+//
+//  Created by Gustavo Anjos on 13/04/21.
+//
+
+import SwiftUI
+
+@main
+struct Cazu_iPhoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
